08f4347493b3b7ab9826b0eb232de53ed161db95
HONGBO <zzf_soft@163.com>
2018-02-01 00:19:52 +0800

    增加远程控制

--- app/src/main/java/com/hongbo/car/MainActivity.java
+++ app/src/main/java/com/hongbo/car/MainActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.contrib.driver.pwmservo.Servo;
+import com.hongbo.car.entity.MoveEntity;
+import com.wilddog.client.ChildEventListener;
+import com.wilddog.client.DataSnapshot;
+import com.wilddog.client.Query;
+import com.wilddog.client.SyncError;
+import com.wilddog.client.SyncReference;
+import com.wilddog.client.WilddogSync;
 
 import java.io.IOException;
 
@@ -19,17 +26,67 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupServo();
-        try {
-            mServo.setAngle(90);
-        } catch (IOException e) {
-            Log.e(TAG, "Error setting the angle", e);
-        }
+        trans(new MoveEntity(6));
+        SyncReference mWilddogRef = WilddogSync.getInstance().getReference().child("move");
+        Query query = mWilddogRef.limitToLast(1);
+        query.addChildEventListener(listener);
     }
+
+    ChildEventListener listener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            Log.d(TAG, "onChildAdded ，" + s);
+            if (dataSnapshot.exists()) {
+                MoveEntity entity = MoveEntity.parse(dataSnapshot.getValue());
+                Log.d(TAG, "MoveEntiry," + entity.moveCode);
+                trans(entity);
+            }
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            Log.d(TAG, "onChildChanged ，" + s);
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            Log.d(TAG, "onChildRemoved");
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            Log.d(TAG, "onChildMoved ，" + s);
+        }
+
+        @Override
+        public void onCancelled(SyncError syncError) {
+            Log.d(TAG, "onCancelled ，" + syncError.getDetails());
+        }
+    };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         destroyServo();
+    }
+
+    private void trans(MoveEntity entity) {
+        MoveEntity.MoveType moveType = MoveEntity.MoveType.getType(entity.moveCode);
+        try {
+            switch (moveType) {
+                case MOVE_TYPE_LEFT:
+                    mServo.setAngle(20);
+                    break;
+                case MOVE_TYPE_RIGHT:
+                    mServo.setAngle(120);
+                    break;
+                case MOVE_TYPE_RUN:
+                    mServo.setAngle(90);
+                    break;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error setting the angle", e);
+        }
     }
 
     private void setupServo() {
---
+++ app/src/main/java/com/hongbo/car/entity/MoveEntity.java
@@ -0,0 +1,57 @@
+package com.hongbo.car.entity;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by HONGBO on 2018/1/31 18:34.
+ */
+public class MoveEntity {
+    public Integer moveCode; //动作编号 1:前进 2：后退 3：左转弯 4：后转弯 5：停止 6：前进
+
+    public MoveEntity(Integer moveCode) {
+        this.moveCode = moveCode;
+    }
+
+    public static MoveEntity parse(Object jsonObj) {
+        return new Gson().fromJson(jsonObj.toString(), MoveEntity.class);
+    }
+
+    public enum MoveType {
+
+        MOVE_TYPE_FORWAD(1),
+        MOVE_TYPE_BACK(2),
+        MOVE_TYPE_LEFT(3),
+        MOVE_TYPE_RIGHT(4),
+        MOVE_TYPE_STOP(5),
+        MOVE_TYPE_RUN(6);
+
+        int code;
+
+        MoveType(int code) {
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public static MoveType getType(int code) {
+            switch (code) {
+                case 1:
+                    return MOVE_TYPE_FORWAD;
+                case 2:
+                    return MOVE_TYPE_BACK;
+                case 3:
+                    return MOVE_TYPE_LEFT;
+                case 4:
+                    return MOVE_TYPE_RIGHT;
+                case 5:
+                    return MOVE_TYPE_STOP;
+                case 6:
+                    return MOVE_TYPE_RUN;
+                default:
+                    return MOVE_TYPE_STOP;
+            }
+        }
+    }
+}
