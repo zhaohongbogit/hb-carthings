777b6f78889c3a4a9834f6034c5e89f4dab440e4
HONGBO <zzf_soft@163.com>
2018-02-13 02:34:23 +0800

    增加后轮电机控制

--- app/src/main/java/com/hongbo/car/MainActivity.java
+++ app/src/main/java/com/hongbo/car/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.contrib.driver.pwmservo.Servo;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.Pwm;
 import com.wilddog.client.ChildEventListener;
 import com.wilddog.client.DataSnapshot;
 import com.wilddog.client.SyncError;
@@ -21,7 +24,18 @@ import tk.hongbo.publicdata.Power;
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    //转向舵机控制
     private static final String PWM_BUS = "PWM1";
+
+    //点击控制部分
+    private static final String EN1_PORT_NAME = "BCM17";
+    private static final String EN2_PORT_NAME = "BCM27";
+    private static final String ENA_PORT_NAME = "PWM0";
+
+    Gpio motorEn1; //输入1针脚
+    Gpio motorEn2; //输入2针脚
+    Pwm motorENA; //使能端A
+
     private Servo mServo;
     SyncReference mWilddogRef;
 
@@ -29,6 +43,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupServo();
+        initGpio(); //初始化电机
         mWilddogRef = WilddogSync.getInstance().getReference().child(Constans.WILDDOG_REF);
         mWilddogRef.addChildEventListener(listener);
     }
@@ -69,10 +84,36 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**
+     * 初始化针脚1引用
+     */
+    private void initGpio() {
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            motorEn1 = service.openGpio(EN1_PORT_NAME);
+            motorEn1.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            motorEn1.setActiveType(Gpio.ACTIVE_HIGH);
+            motorEn1.setValue(true);
+
+            motorEn2 = service.openGpio(EN2_PORT_NAME);
+            motorEn2.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            motorEn2.setActiveType(Gpio.ACTIVE_HIGH);
+            motorEn2.setValue(false);
+
+            motorENA = service.openPwm(ENA_PORT_NAME);
+            motorENA.setPwmFrequencyHz(50);
+            motorENA.setPwmDutyCycle(100);
+            motorENA.setEnabled(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         destroyServo();
+        destoryGpio(); //销毁电机
         mWilddogRef.startAt().removeEventListener(listener);
     }
 
@@ -121,15 +162,68 @@ public class MainActivity extends Activity {
     private void transPower(Power power) {
         switch (power) {
             case POWER_STOP:
+                runMotor(0);
+                runMoto2(0);
                 break;
             case POWER_FORWARD_HIGH:
+                runMotor(1);
+                runMoto2(100);
                 break;
             case POWER_FORWARD_LOW:
+                runMotor(1);
+                runMoto2(30);
                 break;
             case POWER_BACK_HIGH:
+                runMotor(2);
+                runMoto2(100);
                 break;
             case POWER_BACK_LOW:
+                runMotor(2);
+                runMoto2(30);
                 break;
+        }
+    }
+
+    /**
+     * 电机方向控制
+     *
+     * @param forwad
+     */
+    private void runMotor(int forwad) {
+        if (motorEn1 == null || motorEn2 == null) {
+            return;
+        }
+        try {
+            switch (forwad) {
+                case 1:
+                    motorEn1.setValue(false);
+                    motorEn2.setValue(true);
+                    break;
+                case 2:
+                    motorEn1.setValue(true);
+                    motorEn2.setValue(false);
+                    break;
+                default:
+                    motorEn1.setValue(false);
+                    motorEn2.setValue(false);
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 控制电机速度
+     * 0-100
+     *
+     * @param su
+     */
+    private void runMoto2(int su) {
+        try {
+            motorENA.setPwmDutyCycle(su);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -153,6 +247,29 @@ public class MainActivity extends Activity {
             } finally {
                 mServo = null;
             }
+        }
+    }
+
+    /**
+     * 初始化针脚2引用
+     */
+    private void destoryGpio() {
+        try {
+            if (motorEn1 != null) {
+                motorEn1.close();
+            }
+            if (motorEn2 != null) {
+                motorEn2.close();
+            }
+            if (motorENA != null) {
+                motorENA.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            motorEn1 = null;
+            motorEn2 = null;
+            motorENA = null;
         }
     }
 }
